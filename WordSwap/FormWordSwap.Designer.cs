ebd3073f13714c17020b000deb66403f21cca2f6
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-06 15:29:42 +0200

    Add project files.

---
+++ WordSwap/FormWordSwap.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WordSwap
+{
+    partial class FormWordSwap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(41, 58);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(170, 60);
+            this.BtnCalculate.TabIndex = 0;
+            this.BtnCalculate.Text = "Calculate Coins";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(233, 58);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(492, 344);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormWordSwap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCalculate);
+            this.Name = "FormWordSwap";
+            this.Text = "Word Swap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ WordSwap/FormWordSwap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WordSwap
+{
+    public partial class FormWordSwap : Form
+    {
+        public FormWordSwap()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            string word1 = "";
+            string word2 = "";
+
+            string path = Application.StartupPath + @"\words.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int linesCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= linesCount; i++)
+            {
+                word1 = streamReader.ReadLine();
+                word2 = streamReader.ReadLine();
+
+                int coins = CalculateCoins(word1, word2);
+
+                if (coins == 0)
+                {
+                    TxtResult.Text += $"Swapping letters to make {word1} look like {word2} was FREE" + Environment.NewLine;
+                }
+                else if (coins > 0)
+                {
+                    TxtResult.Text += $"Swapping letters to make {word1} look like {word2} earned {coins} coins" + Environment.NewLine;
+                }
+                else
+                {
+                    TxtResult.Text += $"Swapping letters to make {word1} look like {word2} cost {coins * -1} coins" + Environment.NewLine;
+                }
+            }
+        }
+
+        private int CalculateCoins(string word1, string word2)
+        {
+            string alphabet = "0abcdefghijklmnopqrstuvwxyz";
+            string letter1, letter2;
+            int coins = 0;
+
+            for (int i = 0; i < word1.Length; i++)
+            {
+                letter1 = word1.Substring(i, 1);
+                letter2 = word2.Substring(i, 1);
+
+                coins += alphabet.IndexOf(letter1) - alphabet.IndexOf(letter2);
+            }
+            return coins;
+        }
+    }
+}
